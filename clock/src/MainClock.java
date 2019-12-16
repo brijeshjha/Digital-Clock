8b213bf109a8d3ebb1b937c24546cecb48a1d89b
brijeshjha <47074169+brijeshjha@users.noreply.github.com>
2019-12-16 09:26:55 +0530

    Java swing clock app

---
+++ clock/src/MainClock.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JFrame;
+
+
+public class MainClock {
+
+ 
+    public static void main(String[] args) {
+       
+        clockframe cf=new clockframe();
+        cf.setVisible(true);
+        cf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cf.setSize(438,181);
+        
+        
+        
+    }
+    
+}
